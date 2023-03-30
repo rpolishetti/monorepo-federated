231e44b535285fd1c1f39ec29a2909e1a0f37db9
Tech Fxs <techfxs@gmail.com>
2023-03-30 08:32:00 -0700

    Initial workflow

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,26 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js"
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        "marketing": `marketing@${process.env.PRODUCTION_DOMAIN}/marketing/remoteEntry.js`
+      },
+      shared: packageJson.dependencies
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    })
+  ]
+};
+
+module.exports = merge(commonConfig, prodConfig);
